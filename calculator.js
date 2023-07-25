882bcde3d77b35ca6e8bc0291e51b401f7b55da8
Kaua Menezes <kauamenezes6@gmail.com>
2023-07-24 22:37:28 -0300

    minor fixes

---
+++ calculator.js
@@ -0,0 +1,46 @@
+let mathString = '';
+
+$('.calculator-button').on('click', function () {
+  let mathDisplay = $('.text-display')
+  let trimmedButtonText = $(this).text().trim();
+
+
+  if (mathDisplay.text().length > 18) {
+    let maxSize = mathDisplay.text().trim().substring(0, 18);
+
+    $('.limit-alert').addClass('limit-alert-active')
+
+    $('.calculator-button').on('click', function () {
+      if ($(this).hasClass('clear')) {
+        maxSize = '';
+        mathDisplay.text(maxSize);
+      }
+    })
+
+    mathDisplay = maxSize;
+
+    setTimeout(() => {
+      $('.limit-alert').removeClass('limit-alert-active')
+    }, 3000)
+  }
+
+  if ($(this).hasClass('clear')) {
+    mathString = ''
+    mathString = `0`
+    mathDisplay.text(mathString);
+  }
+  else if ($(this).hasClass('result')) {
+    mathDisplay.text(eval(mathString))
+  }
+  else {
+    if (mathDisplay.html() === '0') {
+      mathString = '';
+    }
+
+    mathString += trimmedButtonText;
+    mathDisplay.text(mathString)
+
+    console.log(mathString);
+  }
+
+})
